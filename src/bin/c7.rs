9465f6884b4c33dc90952edefa71203a2df67ee3
Will Hahne <will07c5@gmail.com>
2018-12-12 10:04:15 -0800

    Clean up challenge 7

--- src/bin/c7.rs
+++ src/bin/c7.rs
@@ -6,21 +6,19 @@ extern crate crypto;
 use crypto::aessafe::AesSafe128Decryptor;
 use crypto::symmetriccipher::BlockDecryptor;
 
+const BLOCK_SIZE: usize = 16;
+
 fn main() {
 	let input = challenge_data!("c7.txt");
 	let key = b"YELLOW SUBMARINE";
-	let block_size = 16;
 
-	assert_eq!(key.len(), block_size);
+	assert_eq!(key.len(), BLOCK_SIZE);
 
 	let input_bytes = base64::decode_config(input, base64::MIME).unwrap();
 
 	let decryptor = AesSafe128Decryptor::new(key);
 	let mut plaintext = Vec::new();
-	for block in 0..(input_bytes.len() / block_size) {
-		let block_start = block * block_size;
-		let block_end = (block + 1) * block_size;
-		let input_block = &input_bytes[block_start..block_end];
+	for input_block in input_bytes.chunks(BLOCK_SIZE) {
 		let mut output = [0u8; 16];
 
 		decryptor.decrypt_block(input_block, &mut output);
