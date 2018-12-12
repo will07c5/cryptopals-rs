3e4b7baefcc19e06a4cdca8fc6c2af5ca5fce1a3
Will Hahne <will07c5@gmail.com>
2018-12-12 10:21:01 -0800

    Cleaning up challenge 10

--- src/bin/c10.rs
+++ src/bin/c10.rs
@@ -6,23 +6,20 @@ extern crate crypto;
 use crypto::aessafe::AesSafe128Decryptor;
 use crypto::symmetriccipher::BlockDecryptor;
 
+const BLOCK_SIZE: usize = 16;
+const INPUT: &str = challenge_data!("c10.txt");
+const KEY: &[u8] = b"YELLOW SUBMARINE";
+
 fn main() {
-	let input = challenge_data!("c10.txt");
-	let key = b"YELLOW SUBMARINE";
-	const block_size: usize = 16;
+	assert_eq!(KEY.len(), BLOCK_SIZE);
 
-	assert_eq!(key.len(), block_size);
+	let input_bytes = base64::decode_config(INPUT, base64::MIME).unwrap();
 
-	let input_bytes = base64::decode_config(input, base64::MIME).unwrap();
-
-	let decryptor = AesSafe128Decryptor::new(key);
+	let decryptor = AesSafe128Decryptor::new(KEY);
 	let mut plaintext = Vec::new();
-	let mut prev_block = [0u8; block_size];
-	for block in 0..(input_bytes.len() / block_size) {
-		let block_start = block * block_size;
-		let block_end = (block + 1) * block_size;
-		let input_block = &input_bytes[block_start..block_end];
-		let mut output = [0u8; block_size];
+	let mut prev_block = [0u8; BLOCK_SIZE];
+	for input_block in input_bytes.chunks(BLOCK_SIZE) {
+		let mut output = [0u8; BLOCK_SIZE];
 
 		decryptor.decrypt_block(input_block, &mut output);
 
@@ -31,7 +28,6 @@ fn main() {
 		prev_block[0..].copy_from_slice(input_block);
 
 		plaintext.append(&mut plain_block);
-
 	}
 
 	let plaintext_str = String::from_utf8(plaintext).unwrap();
