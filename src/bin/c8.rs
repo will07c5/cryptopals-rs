f598bd5154381b5db4a47367cff957635a73c2ab
Will Hahne <will07c5@gmail.com>
2018-12-12 10:16:12 -0800

    Modify challenge 8 to use chunks

--- src/bin/c8.rs
+++ src/bin/c8.rs
@@ -2,25 +2,21 @@
 extern crate common;
 extern crate hex;
 
+const BLOCK_SIZE: usize = 16;
+
 fn main() {
 	let input = challenge_data!("c8.txt");
-	let block_size = 16;
 
 	let mut found = None;
 
 	'outer: for (line_num, line) in input.lines().enumerate() {
 		let bytes = hex::decode(line).unwrap();
-		let block_count = bytes.len() / block_size;
-		assert_eq!(block_count * block_size, bytes.len());
+		let block_count = bytes.len() / BLOCK_SIZE;
+		assert_eq!(bytes.len() % BLOCK_SIZE, 0);
 
-		for block1 in 0..(block_count-1) {
-			for block2 in (block1+1)..block_count {
-				let block1_start = block1 * block_size;
-				let block1_end = (block1 + 1) * block_size;
-				let block2_start = block2 * block_size;
-				let block2_end = (block2 + 1) * block_size;
-
-				if &bytes[block1_start..block1_end] == &bytes[block2_start..block2_end] {
+		for (chunk_idx, block1) in bytes.chunks(BLOCK_SIZE).enumerate() {
+			for block2 in bytes[(chunk_idx + 1) * BLOCK_SIZE..].chunks(BLOCK_SIZE) {
+				if block1 == block2 {
 					println!("Line = {}", line_num);
 					assert_eq!(found, None);
 					found = Some(line);
