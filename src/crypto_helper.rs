f68029df60f9b11ffb4099998d7b88ade06eff48
Will Hahne <will07c5@gmail.com>
2018-12-13 15:40:44 -0800

    Add ecb encrypt/decrypt functions

--- src/crypto_helper.rs
+++ src/crypto_helper.rs
@@ -1,6 +1,7 @@
 // convenience wrappers for crypto library
 
 use crypto::aes::{cbc_encryptor, cbc_decryptor, KeySize};
+use crypto::aes::{ecb_encryptor, ecb_decryptor};
 use crypto::blockmodes::NoPadding;
 use crypto::buffer::{RefReadBuffer, RefWriteBuffer, ReadBuffer, WriteBuffer};
 use crypto::aessafe::AesSafe128Encryptor;
@@ -33,6 +34,36 @@ pub fn decrypt_cbc(key: &[u8], iv: &[u8], input: &[u8]) -> Vec<u8> {
     let mut output = vec!(0u8; input.len());
 
     let mut decryptor = cbc_decryptor(KeySize::KeySize128, &key, &iv, NoPadding);
+    let mut input_buf = RefReadBuffer::new(&input);
+    let mut output_buf = RefWriteBuffer::new(&mut output);
+
+    decryptor.decrypt(&mut input_buf, &mut output_buf, true).unwrap();
+
+    output_buf.take_read_buffer().take_remaining().to_vec()
+}
+
+pub fn encrypt_ecb(key: &[u8], input: &[u8]) -> Vec<u8> {
+    assert_eq!(key.len(), BLOCK_SIZE);
+    assert_eq!(input.len() % BLOCK_SIZE, 0);
+
+    let mut output = vec!(0u8; input.len());
+
+    let mut encryptor = ecb_encryptor(KeySize::KeySize128, &key, NoPadding);
+    let mut input_buf = RefReadBuffer::new(&input);
+    let mut output_buf = RefWriteBuffer::new(&mut output);
+
+    encryptor.encrypt(&mut input_buf, &mut output_buf, true).unwrap();
+
+    output_buf.take_read_buffer().take_remaining().to_vec()
+}
+
+pub fn decrypt_ecb(key: &[u8], input: &[u8]) -> Vec<u8> {
+    assert_eq!(key.len(), BLOCK_SIZE);
+    assert_eq!(input.len() % BLOCK_SIZE, 0);
+
+    let mut output = vec!(0u8; input.len());
+
+    let mut decryptor = ecb_decryptor(KeySize::KeySize128, &key, NoPadding);
     let mut input_buf = RefReadBuffer::new(&input);
     let mut output_buf = RefWriteBuffer::new(&mut output);
 
