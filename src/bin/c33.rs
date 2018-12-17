735d369dee1ae712cb4bf5eb9f1bdceff2f374ee
Will Hahne <will07c5@gmail.com>
2018-12-16 21:07:38 -0800

    Completed challenge 33

---
+++ src/bin/c33.rs
@@ -0,0 +1,61 @@
+extern crate common;
+extern crate rand;
+extern crate num_bigint;
+
+use rand::Rng;
+use num_bigint::{ToBigUint, RandBigInt, BigUint};
+
+const P: &[u8] = b"ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024\
+e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd\
+3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec\
+6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f\
+24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361\
+c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552\
+bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff\
+fffffffffffff";
+
+const G: usize = 2;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+
+    {
+        let p: u128 = 37;
+        let g: u128 = 5;
+
+        let a_priv = rng.gen_range::<u32>(0, p as u32);
+        let a_pub = g.pow(a_priv) % p;
+        println!("a pub/priv {}/{}", a_pub, a_priv);
+
+        let b_priv = rng.gen_range::<u32>(0, p as u32);
+        let b_pub = g.pow(b_priv) % p;
+        println!("b pub/priv {}/{}", b_pub, b_priv);
+
+        let s_a = b_pub.pow(a_priv) % p;
+        let s_b = a_pub.pow(b_priv) % p;
+        println!("s a/b {}/{}", s_a, s_b);
+
+        assert_eq!(s_a, s_b);
+    }
+
+    {
+        let p = BigUint::parse_bytes(&P, 16).unwrap();
+        let g = G.to_biguint().unwrap();
+        let zero = 0.to_biguint().unwrap();
+
+        let a_priv = rng.gen_biguint_range(&zero, &p);
+        let a_pub = g.modpow(&a_priv, &p);
+        println!("a pub/priv {}/{}", a_pub, a_priv);
+
+        let b_priv = rng.gen_biguint_range(&zero, &p);
+        let b_pub = g.modpow(&b_priv, &p);
+        println!("b pub/priv {}/{}", b_pub, b_priv);
+
+        let s_a = b_pub.modpow(&a_priv, &p);
+        let s_b = a_pub.modpow(&b_priv, &p);
+        println!("s a/b {}/{}", s_a, s_b);
+
+        assert_eq!(s_a, s_b);
+    }
+
+}
